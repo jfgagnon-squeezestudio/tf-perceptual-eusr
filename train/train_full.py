195b18349b677101881c342421722ca0d77e7d26
jfgagnon <jfgagnon_00@yahoo.com>
2020-01-01 15:21:23 -0500

    Training update * Added pre trained x2 model. Got them from authors. * Updated training wrapper script in case we want to do everything by hand

--- train/train_full.py
+++ train/train_full.py
@@ -1,9 +1,18 @@
 import os
+import platform
 from tensorflow.python.client import device_lib
 
 # validate setup before
+print("Tensorflow test")
 print(device_lib.list_local_devices())
+
+pythonExec = "py" if (platform.system() == "Windows") else "python"
 
 # data is assumed to be present and valid
 # Pre-train EUSR model
-# os.system("py train.py --data_input_path=./LR --data_truth_path=./HR --model=eusr --scales=2,4")
+eusrModelTrainingArguments = "train.py --data_input_path=./LR --data_truth_path=./HR --model=eusr --scales=2 --sleep_ratio=0.0 --save_freq=50 --log_freq=2"
+pythonCmdLine = f"{pythonExec} {eusrModelTrainingArguments}"
+
+print("Python command line:")
+print(pythonCmdLine)
+os.system(pythonCmdLine)
